1ba319e8f4f9de4a14c46057f4e57c9dfa6834e1
Goutham <gouthamkumarpolapally@gmail.com>
2024-05-02 18:34:58 +0530

    scripts added

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ const razorPay = require("./razorpay")
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use("/api/razorpay",razorPay)
-app.listen(port,(port)=>{console.log("Server running on port "+port)})
+app.listen(port,()=>{console.log("Server running on port "+port)})
